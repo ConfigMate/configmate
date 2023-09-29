d12b7be5e28114a63e63ce288528f56dfe17d0f6
Julio J. Cabrera <jcabza008@gmail.com>
2023-09-29 14:02:12 -0400

    Added definition of analyzer output

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -1,7 +1,18 @@
 package analyzer
 
 type Analyzer interface {
-	Analyze(rb Rulebook) (res Result, err error)
+	Analyze(rb Rulebook) (res []Result, err error)
 }
 
-type Result struct{}
+type Result struct {
+	Passed        bool            `json:"passed"`         // true if the check passed, false if it failed
+	ResultComment string          `json:"result_comment"` // an error msg or comment about the result
+	TokenList     []TokenLocation `json:"token_list"`     // a list of tokens that were involved in the rule
+}
+
+type TokenLocation struct {
+	File   string `json:"file"`
+	Line   int    `json:"line"`
+	Column int    `json:"column"`
+	Length int    `json:"length"`
+}
