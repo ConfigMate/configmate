7c5e93791389675ec108808da8e4110744a57603
Julio J. Cabrera <34218922+Jcabza008@users.noreply.github.com>
2023-10-04 13:37:21 -0400

    feature/wrote-cli-interface (#40)
    Closes #31.

--- go.mod
+++ go.mod
@@ -4,4 +4,13 @@ go 1.19
 
 require github.com/antlr4-go/antlr/v4 v4.13.0
 
-require golang.org/x/exp v0.0.0-20230515195305-f3d0a9c9a5cc // indirect
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+)
+
+require (
+	github.com/urfave/cli/v2 v2.25.7
+	golang.org/x/exp v0.0.0-20230515195305-f3d0a9c9a5cc // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,95 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+// Version contains the semver version number for the controller. (set via ldflags during build)
+var Version = "0.0.0-none"
+
+// BuildDate contains the build date and time in RFC 3339 format. (set via ldflags during build)
+var BuildDate = "Not Provided"
+
+// GitHash contains the Git commit hash from which the controller was built. (set via ldflags during build)
+var GitHash = "Not Provided"
 
 func main() {
-	fmt.Println("Not Implemented.")
+	app := &cli.App{
+		Name:                 "configm",
+		Usage:                "Tool to check configuration files for errors in content.",
+		UsageText:            "configm [global options] command [command options] [arguments...]",
+		EnableBashCompletion: true,
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "verbose",
+				Aliases: []string{"v"},
+				Usage:   "Enable verbose output.",
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:      "version",
+				Usage:     "Print version information.",
+				UsageText: "configm version",
+				Action: func(c *cli.Context) error {
+					// Print version information
+					fmt.Printf("Version: %s\n", Version)
+
+					// Print build information if verbose flag is set
+					if c.Bool("verbose") {
+						fmt.Printf("Build Date: %s\n", BuildDate)
+						fmt.Printf("Git Hash: %s\n", GitHash)
+					}
+
+					return nil
+				},
+			},
+			{
+				Name:      "check",
+				Usage:     "Check configuration files for errors in content.",
+				UsageText: "configm check --rulebook <rulebook>",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "rulebook",
+						Aliases:  []string{"r"},
+						Usage:    "Rulebook to use for checking.",
+						Required: true,
+					},
+				},
+				Action: func(c *cli.Context) error {
+					// Implement check command
+					return nil
+				},
+			},
+			{
+				Name:      "serve",
+				Usage:     "Start a web server to check configuration files for errors in content.",
+				UsageText: "configm serve --port <port>",
+				Flags: []cli.Flag{
+					&cli.IntFlag{
+						Name:    "port",
+						Aliases: []string{"p"},
+						Usage:   "Port to listen on.",
+						Value:   10007,
+					},
+				},
+				Action: func(c *cli.Context) error {
+					// Implement serve command
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		// Print error message
+		fmt.Printf("Error: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	// Exit with success
+	os.Exit(0)
 }
