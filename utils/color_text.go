e2be3d597f6eb743c52936f731b86531337b7c05
Julio J. Cabrera <jcabza008@gmail.com>
2023-10-19 20:49:44 -0400

    make lint fixes

--- utils/color_text.go
+++ utils/color_text.go
@@ -3,19 +3,19 @@ package utils
 type StdOutColor string
 
 const (
-    Red StdOutColor = "\033[31m" 
-    Green  = "\033[32m"
-    Yellow = "\033[33m"
-    Blue   = "\033[34m"
-    Purple = "\033[35m"
-    Cyan   = "\033[36m"
-    Gray   = "\033[37m"
-    White  = "\033[97m"
+	Red    StdOutColor = "\033[31m"
+	Green  StdOutColor = "\033[32m"
+	Yellow StdOutColor = "\033[33m"
+	Blue   StdOutColor = "\033[34m"
+	Purple StdOutColor = "\033[35m"
+	Cyan   StdOutColor = "\033[36m"
+	Gray   StdOutColor = "\033[37m"
+	White  StdOutColor = "\033[97m"
 )
 
-const Reset  = "\033[0m"
+const Reset string = "\033[0m"
 
 func ColorText(text string, color StdOutColor) string {
-    value := string(color) + text + string(Reset)
-    return value
+	value := string(color) + text + string(Reset)
+	return value
 }
