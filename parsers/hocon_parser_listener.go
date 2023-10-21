756a871518d5c654ad5cec47c2ecf0492d68d967
Javier Arango <javierarango.developer@gmail.com>
2023-10-21 17:31:33 -0400

    feature/hocon-parser (#57)
    ## Description
    - Added HOCON grammar for the custom parser
    - Made a template for the custom HOCON parser
    - Created a basic HOCON parser that just parse the file without
    returning any custom data structure
    - Made unit test:
        - Check for SyntaxError
        - Check for basic functionality of the parser
    
    ## References
    I found the grammar in this
    [repo](https://github.com/jdevelop/go-hocon/blob/master/parser/HOCON.g4)

---
+++ parsers/hocon_parser_listener.go
@@ -0,0 +1,59 @@
+package parsers
+
+import (
+	"fmt"
+
+	parser_hocon "github.com/ConfigMate/configmate/parsers/gen/parser_hocon/parsers/grammars"
+	"github.com/antlr4-go/antlr/v4"
+	// "github.com/golang-collections/collections/stack"
+)
+
+type HoconParser struct{}
+
+type HoconErrorListener struct {
+	antlr.DefaultErrorListener
+	errors []error
+}
+
+// Error handling
+func (s *HoconErrorListener) SyntaxError(recognizer antlr.Recognizer, offendingSymbol interface{}, line, column int, msg string, e antlr.RecognitionException) {
+	s.errors = append(s.errors, fmt.Errorf("line %d:%d %s", line, column, msg))
+}
+
+// Custom Json Parser
+func (p *HoconParser) Parse(data []byte) (*Node, error) {
+	// Initialize the error listener
+	errorListener := &HoconErrorListener{}
+
+	input := antlr.NewInputStream(string(data))
+	lexer := parser_hocon.NewHOCONLexer(input)
+
+	// Attach the error listener to the lexer
+	lexer.AddErrorListener(errorListener)
+
+	tokenStream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+	parser := parser_hocon.NewHOCONParser(tokenStream)
+
+	// Attach the error listener to the parser
+	parser.AddErrorListener(errorListener)
+
+	tree := parser.Hocon()
+
+	// Check for errors after parsing
+	if len(errorListener.errors) > 0 {
+		return nil, fmt.Errorf("Syntax errors: %v", errorListener.errors)
+	}
+
+	walker := antlr.NewParseTreeWalker()
+	hoconListener := &HoconParserListener{}
+	walker.Walk(hoconListener, tree)
+
+	return hoconListener.configFile, nil
+}
+
+type HoconParserListener struct {
+	*parser_hocon.BaseHOCONListener
+
+	configFile *Node
+	// stack      stack.Stack
+}
