274447e7728fea41f4ac14b545cb62d2cdeae817
Jean Gregor Fonrose <fonrosejeangregor@gmail.com>
2023-10-23 19:09:45 -0400

    added the files for printing results and testing of the functionality

---
+++ utils/print_results.go
@@ -0,0 +1,9 @@
+package utils
+
+import (
+	"fmt"
+)
+
+func PrintResults() {
+	fmt.Print("I am gonna write some nice results")
+}
---
+++ utils/print_results_test.go
@@ -0,0 +1,9 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestPrintResults(t *testing.T) {
+	
+}
